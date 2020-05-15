bf909d762e374e061b81dfe45c747a56fc9ba8bc
Mattia Vorstenbosch <33621526+MattiaVorstenbosch@users.noreply.github.com>
2020-05-15 12:19:00 +0200

    Initial commit

---
+++ build/precache-manifest.453aebf3b8f23f013aa02e021d39fb29.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4805e3505c3bdb20e6ea146bf95c4a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c7dc37795331f0ef34a",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5a5115b83688696dd272",
+    "url": "/static/js/2.22abec0c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.22abec0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c7dc37795331f0ef34a",
+    "url": "/static/js/main.a5ac81fa.chunk.js"
+  },
+  {
+    "revision": "1f7e1dd6bb661d74a9b4",
+    "url": "/static/js/runtime-main.a461276e.js"
+  }
+]);
